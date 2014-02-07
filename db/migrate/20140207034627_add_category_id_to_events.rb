8346e16362f3642723b592ad24868e89ab207442
Alex Egan <aegan@andrew.cmu.edu>
2014-02-06 22:46:54 -0500

    adds categories

---
+++ db/migrate/20140207034627_add_category_id_to_events.rb
@@ -0,0 +1,5 @@
+class AddCategoryIdToEvents < ActiveRecord::Migration
+  def change
+    add_column :events, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203051518) do
+ActiveRecord::Schema.define(version: 20140207034627) do
 
   create_table "attendances", force: true do |t|
     t.integer  "user_id"
     t.integer  "event_id"
     t.boolean  "confirmed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140203051518) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
   create_table "users", force: true do |t|
