f36078e12fff9dfe23dfed68c14827d476548fa1
Alex Egan <aegan@andrew.cmu.edu>
2014-02-07 21:31:55 -0500

    adds category model code, adds category_id relationship and validations in event.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,14 @@
 class Category < ActiveRecord::Base
+	# relationships
+	has_many :events
+
+	# validations
+	validates_presence_of :name
+	validates_uniquness_of :name, case_sensitive: false
+	validates_inclusion_of :active, in: [true, false]
+
+	# scopes
+	scope :alphabetical, -> { order('name') }
+	scope :active, -> { where(active: true) }
+	scope :inactive, -> { where(active: false) }
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,13 @@
 class Event < ActiveRecord::Base
+	# relationships
 	belongs_to :creator, class_name: "User", foreign_key: "user_id"
+	belongs_to :category
 	has_many :attendances
 	has_many :users, through: :attendances
 
 	# validations
 	validates_numericality_of :user_id, only_integer: true, greater_than: 0, allow_blank: false
+	validates_numericality_of :category_id, only_integer: true, greater_than: 0, allow_blank: false
 	validates_presence_of :name
 	validates_presence_of :description
 	validates_inclusion_of :active, in: [true, false], message: "Must be true or false"
@@ -14,6 +17,7 @@ class Event < ActiveRecord::Base
 	validates_time :start_time, on_or_after: Time.now-2.seconds, on: :create, allow_blank: false
 	validates_time :end_time, on_or_after: :start_time, allow_blank: true
 	validate :creator_in_system
+	validate :category_active_in_system
 
 	# scopes
 	# can add more later
@@ -30,7 +34,16 @@ class Event < ActiveRecord::Base
 	def creator_in_system
 		creator_ids = User.all.map { |user| user.id }
 		unless creator_ids.include?(self.user_id)
-			errors.add(:creator, 'is not active in the system')
+			errors.add(:creator, 'is not recognized by the system')
+			return false
+		end
+		true
+	end
+	# make sure category is active in the system
+	def category_active_in_system
+		category_ids = Category.active.map { |category| category.id }
+		unless category_ids.include?(self.category_id)
+			errors.add(:category, ' is not active in the system')
 			return false
 		end
 		true
