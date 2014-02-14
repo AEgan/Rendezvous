563020114d7e6a7e11999380fbb0745b9c41fddd
Alex Egan <aegan@andrew.cmu.edu>
2014-02-14 01:12:05 -0500

    modifies event index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.by_start_time.active
   end
 
   # GET /events/1
